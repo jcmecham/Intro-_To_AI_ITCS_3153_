37790d8db7d923a54da02556bdf527475185770a
jcmecham <60199464+jcmecham@users.noreply.github.com>
2022-02-01 16:58:17 -0500

    initialize checkGoalState

--- Assignment1.java
+++ Assignment1.java
@@ -3,32 +3,22 @@ import java.util.ArrayList;
 public class Assignment1{
   public static int boardSize = 10;
 
-  public static void printBoard(ArrayList<ArrayList<Integer>> board){
-    for(int y = 0; y < boardSize; y++){
-      for(int x = 0; x < boardSize; x++){
-        System.out.print(board.get(y).get(x).toString());
-        if(x != boardSize - 1){
-          System.out.print(",");
-        }else{
-          System.out.println("");
-        }
-      }
-    }
-  }
-
+  
+  public static void printBoard(){};
+  public static void checkGoalState(){};
   public static void main(String[] args){
     ArrayList<ArrayList<Integer>> board = new ArrayList<ArrayList<Integer>>(); //create 2d arrays
     ArrayList<ArrayList<Integer>> heuristicBoard = new ArrayList<ArrayList<Integer>>();
 
 
-    //initializes 2d arraylist 
+    //initializes 2d arraylist for board 
     for(int a = 0; a < boardSize; a++){
       board.add(new ArrayList<Integer>(2));
       for(int b = 0;b < boardSize; b++){
         board.get(a).add(0);
       }
     }
-    //initializes 2d arraylist 
+    //initializes 2d arraylist for heuristicBoard
     for(int a = 0; a < boardSize; a++){
       heuristicBoard.add(new ArrayList<Integer>(2));
       for(int b = 0;b < boardSize; b++){
@@ -47,8 +37,28 @@ public class Assignment1{
     printBoard(heuristicBoard);
   }
 
+  public static void printBoard(ArrayList<ArrayList<Integer>> board){
+    for(int y = 0; y < boardSize; y++){
+      for(int x = 0; x < boardSize; x++){
+        System.out.print(board.get(y).get(x).toString());
+        if(x != boardSize - 1){
+          System.out.print(",");
+        }else{
+          System.out.println("");
+        }
+      }
+    }
+  }
 
-  
+  public static void checkGoalState(ArrayList<ArrayList<Integer>> board){
+    for(int y = 0; y < boardSize; y++){
+      for(int x = 0; x < boardSize; x++){
+
+      }
+    }
+  }
+
+
 }
 
 
